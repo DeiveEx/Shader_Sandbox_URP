a7c751908e1cddd13e5fb008d9293595607a4dfa
Deive_Ex <feliperobos@gmail.com>
2019-09-30 21:26:22 -0300

    Converted to 2019.2.6 and Cracked Ice

---
+++ Assets/_Shaders/Custom Functions/TestCustomFunction.hlsl
@@ -0,0 +1,10 @@
+﻿#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+//The name of the function must the EXACTLY the same as the name of the function. The function MUST have a precision suffix (the "_float"), but it MUST NOT be in the name of the function on the custom function node. (In this case, the node will call a function named "MyFunction" instead of "MyFunction_float")
+void MyFunction_float(float A, float B, out float Out1, out float Out2) {
+	Out1 = 0;
+	Out2 = 1;
+}
+
+#endif
