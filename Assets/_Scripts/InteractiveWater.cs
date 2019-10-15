2a21c99fd99d6aa4dd62f0692a0e58a18e4ba55c
Deive_Ex <feliperobos@gmail.com>
2019-10-14 23:28:40 -0300

    Interactive water

---
+++ Assets/_Scripts/InteractiveWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveWater : MonoBehaviour
+{
+	[SerializeField]
+	RenderTexture renderTexture;
+	[SerializeField]
+	Transform target;
+
+	private Transform myTransform;
+	private float posY;
+
+	private void Awake() {
+		myTransform = transform;
+		posY = myTransform.position.y;
+
+		Shader.SetGlobalTexture("_WaterRippleRT", renderTexture);
+		Shader.SetGlobalFloat("_OrtographicCameraSize", GetComponent<Camera>().orthographicSize);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		myTransform.position = target.position + (Vector3.up * posY);
+		Shader.SetGlobalVector("_Position", myTransform.position);
+    }
+}
---
+++ Assets/_Scripts/SimplePlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+	public float force;
+
+	private Rigidbody rb;
+	private Vector3 input;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		if(input.x != 0 || input.z != 0) {
+			rb.AddForce(input * force);
+		}
+    }
+}
