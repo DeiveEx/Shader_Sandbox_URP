b3cf5a68fa28c7ca0f468e8ee975cf49c2578dc5
Deive_Ex <feliperobos@gmail.com>
2024-04-06 02:05:08 -0300

    Renderer Features

--- Assets/_RenderFeatures/SimpleBlit/SimpleBlitFeature.cs
+++ Assets/_RenderFeatures/SimpleBlit/SimpleBlitFeature.cs
@@ -60,7 +60,7 @@ public class SimpleBlitFeature : ScriptableRendererFeature
 	//These fields will appear in the inspector of the Render Feature
 	[SerializeField] private RenderPassEvent renderEvent = RenderPassEvent.AfterRenderingPostProcessing;
 	[SerializeField] private Material blitMaterial; //The material to use for the blit
-	[SerializeField] private int passIndex; //The pass to be used by the shader (Shader Graphs shoul always use "0")
+	[SerializeField] private int passIndex; //The pass to be used by the shader (Shader Graphs should always use "0")
 
 	CustomRenderPass m_ScriptablePass;
 
@@ -72,11 +72,14 @@ public class SimpleBlitFeature : ScriptableRendererFeature
 		m_ScriptablePass.renderPassEvent = renderEvent;
 	}
 
-	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+	public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
 	{
 		//Get the renderer color texture and pass it as the source of our pass
-		m_ScriptablePass.SetSource(renderer.cameraColorTarget);
+		m_ScriptablePass.SetSource(renderer.cameraColorTargetHandle);
+	}
 
+	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+	{
 		//Enqueue the pass
 		renderer.EnqueuePass(m_ScriptablePass);
 	}
